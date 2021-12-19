48dd5f7c888930895e756b67ec4b20ed29c5ca03
ShobhanSrivastava <shobhansrivastava.200121067@gehu.ac.in>
2021-12-19 18:32:39 +0530

    Routes added

--- node-js-app/app.js
+++ node-js-app/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware")
-    next()
-})
+app.use(bodyParser.urlencoded({ extended : false }));
 
-app.use((req, res, next) => {
-    console.log("In another middleware")
-})
+const adminRoutes = require('../node-js-app/routes/admin');
+
+app.use(adminRoutes);
+
+/* The app listens for the request and the request checks for the path just like if else 
+in any other programming language. With the app.use function if the part of the path of the request is contained in 
+the given path in the the app.use function, the function gets executed. */
+
+app.use('/',(req, res, next) => { //This works like an else block, if nothing works, this will be executed
+    res.send("<h1>Home Page</h1>")
+});
 
 app.listen(3000);
