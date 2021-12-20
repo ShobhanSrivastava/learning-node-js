fae9764344f93a3fc2a9f8b3444431c724e3258b
ShobhanSrivastava <shobhansrivastava.200121067@gehu.ac.in>
2021-12-20 10:44:52 +0530

    Serving CSS Files Statically

--- node-js-app/app.js
+++ node-js-app/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended : false }));
 
@@ -12,7 +15,7 @@ app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send("<h1>Page not Found</h1>");
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
 
 app.listen(3000);
--- node-js-app/routes/shop.js
+++ node-js-app/routes/shop.js
@@ -1,13 +1,12 @@
-const app = require('express');
-
-const path = require('path');
-
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
+const rootDir = require('../utils/path')
+
 router.get('/',(req, res, next) => { //This works like an else block, if nothing works, this will be executed
-    res.sendFile(path.join(__dirname,'/..','views','shop.html'));
+    res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
