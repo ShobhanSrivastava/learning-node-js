e71ea2fbff226d421d58cf66579b8b969a7f7616
ShobhanSrivastava <shobhansrivastava.200121067@gehu.ac.in>
2021-12-21 20:38:28 +0530

    Dynamic Content using PUG

--- node-js-app/routes/admin.js
+++ node-js-app/routes/admin.js
@@ -3,15 +3,17 @@ const path = require('path');
 
 const router = express.Router();
 
+const products = [];
 //admin/add-product => GET
 router.get('/add-product',(req, res, next) => {
-    res.sendFile(path.join(__dirname,'/..','views','add-products.html'))
+    res.render('add-products',{ title : "Add Products" })
 });
 
 //admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
+    products.push({title : req.body.title})
     res.redirect("/");
 });
 
-module.exports = router;
+module.exports = { routes : router, prods : products};
--- node-js-app/routes/shop.js
+++ node-js-app/routes/shop.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const path = require('path');
 
 const router = express.Router();
 
-const rootDir = require('../utils/path')
+const rootDir = require('../utils/path');
+const { prods } = require('./admin');
+const adminData = require('./admin')
 
 router.get('/',(req, res, next) => { //This works like an else block, if nothing works, this will be executed
-    res.sendFile(path.join(rootDir, "views", "shop.html"));
+    const products = adminData.prods;
+    res.render('shop', { prod : prods, pageTitle : 'Shop' });
 });
 
 module.exports = router;
